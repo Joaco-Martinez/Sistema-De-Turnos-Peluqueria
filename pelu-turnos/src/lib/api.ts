3dda9707183427bc1f3e058f76b66f8630efa404
Joaquin Martinez <joaco.martinez1480@gmail.com>
2025-08-18 16:33:15 -0300

    Delete pelu-turnos directory

--- pelu-turnos/src/lib/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import { getApiBaseURL } from "./baseUrl";
-
-export const api = axios.create({
-  baseURL: getApiBaseURL(),
-  headers: { "Content-Type": "application/json" }
-});
